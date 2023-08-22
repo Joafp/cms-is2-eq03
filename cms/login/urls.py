8e1722a86779f3fee645fe18914e86220b073a0a
CarlosAyala00 <ayalabicho018@gmail.com>
2023-08-22 15:08:45 -0400

    commit de inicio de sesion autentificado

--- cms/login/urls.py
+++ cms/login/urls.py
@@ -4,7 +4,6 @@ from core import views as menu
 
 urlpatterns = [
    path('',views.vista_login,name='login'),  
-   path('registro/',views.registro,name='registro'),
    path('menuprincipal/',menu.vista_MenuPrincipal,name='MenuPrincipal'),
 ] 
  
