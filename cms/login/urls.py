77f5282a36511e87af5e9f7850e46d14a8002bfa
Joafp <joad.d@fpuna.edu.py>
2023-08-22 12:07:57 -0400

    redireccionamiento a crear usuario

--- cms/login/urls.py
+++ cms/login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-   path('',views.vista_login,name='login'),     
-]
+   path('',views.vista_login,name='login'),  
+   path('registro/',views.registro,name='registro')
+] 
  
