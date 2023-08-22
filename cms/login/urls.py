5b2e8fd4161884c4d920c73ddb20985c505132dd
Joafp <joad.d@fpuna.edu.py>
2023-08-22 12:24:11 -0400

    configuracion menu principal

--- cms/login/urls.py
+++ cms/login/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from core import views as menu
+
 urlpatterns = [
    path('',views.vista_login,name='login'),  
-   path('registro/',views.registro,name='registro')
+   path('registro/',views.registro,name='registro'),
+   path('menuprincipal/',menu.vista_MenuPrincipal,name='MenuPrincipal'),
 ] 
  
