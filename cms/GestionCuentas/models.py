29cb1eb12be699a63bf57dde7a69845cc751c128
Joafp <joad.d@fpuna.edu.py>
2023-08-23 14:33:52 -0400

    Agregue que cuando se crea un usuario nuevo se le asigne el rol suscriptor por defecto

--- cms/GestionCuentas/models.py
+++ cms/GestionCuentas/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import Permission
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import Permission, User
 class PermisosPer(models.Model):
     class Meta:
         permissions=[
