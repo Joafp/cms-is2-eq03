673225438af5619060e3554cf51fc2f5a5b0c4de
Joafp <joad.d@fpuna.edu.py>
2023-08-23 17:01:45 -0400

    testing de gestion de cuentas

--- cms/GestionCuentas/tests.py
+++ cms/GestionCuentas/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Rol, UsuarioRol
+class RolTestCase(TestCase):
+    def crear(self):
+        Rol.objects.create(name="Rol_Prueba")
+    def test_nombre(self):
+        item=Rol.objects.get(name="Rol_Prueba")
+        self.assertEqual(item.nombre,"Rol_Prueba")
 
-# Create your tests here.
