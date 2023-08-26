4d52fbf19e0bdb1afe5088c71e5288beb353a771
Joafp <joad.d@fpuna.edu.py>
2023-08-26 19:00:25 -0400

    borre archivos

--- cms/core/urls.py
+++ cms/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('menuprincipal/',views.vista_MenuPrincipal,name='MenuPrincipal'),
+    path('',views.vista_MenuPrincipal,name='MenuPrincipal'),
     path('main_trabajador/',views.vista_trabajador,name='maintrabajador'),
-    path('',include('login.urls')),
+    path('login/',include('login.urls')),
 ]
