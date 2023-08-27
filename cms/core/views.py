f7afb9a4c8f25c2492a29922062ecca51407955b
Joafp <joad.d@fpuna.edu.py>
2023-08-26 20:23:48 -0400

    Cambie que la pagina principal sea la pagina de contenidos y que haya un boton de inicio de sesion

--- cms/core/views.py
+++ cms/core/views.py
@@ -3,11 +3,15 @@ from django.contrib.auth.decorators import login_required
 from GestionCuentas.models import UsuarioRol
 from django.contrib.auth.models import User
 from django.views.decorators.cache import never_cache
-@login_required(login_url="/login")
 @never_cache
 def vista_MenuPrincipal(request):
-    usuario_rol = UsuarioRol.objects.get(username=request.user.username)
-    return render(request, 'crear/main.html', {'usuario_rol': usuario_rol})
+    autenticado=User.is_authenticated
+    #usuario_rol = UsuarioRol.objects.get(username=request.user.username)
+    context={
+        'autenticado':autenticado,
+    }
+    print("Usuario: ",autenticado)
+    return render(request, 'crear/main.html',context )
 @login_required(login_url="/login")
 def vista_trabajador(request):
     usuario_rol = UsuarioRol.objects.get(username=request.user.username)
