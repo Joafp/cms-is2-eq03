e6e2e78b479f63bf52130a0116c1388a7ea0d546
Joafp <joad.d@fpuna.edu.py>
2023-09-07 17:13:54 -0400

    Config del nginx y cambio de html

--- cms/core/urls.py
+++ cms/core/urls.py
@@ -6,6 +6,7 @@ from .views import CrearContenido,VistaArticulos,VistaContenidos
 from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.contrib import admin
 urlpatterns = [
     path('',views.vista_MenuPrincipal,name='MenuPrincipal'),
     path('main_trabajador/',views.vista_trabajador,name='maintrabajador'),
@@ -20,6 +21,7 @@ urlpatterns = [
     path('roles/',views.vista_roles, name='gestion'),
     path('asignar/',views.asignar_rol,name='asignacion'),
     path('desasignar/',views.remover_rol,name='desasignar'),
+    path('admin/', admin.site.urls),
 ]
 """Nos permite vincular la direccion donde tenemos guardadas nuestras imagenes, en este caso
 tenemos las imagenes en la carpeta raiz y esta esta definida en el archivo settings"""
